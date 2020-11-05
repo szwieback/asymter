89275e328d15c38f2ab4d02b673414c73bc3ac2f
Simon Zwieback <szwieback@alaska.edu>
2020-11-04 16:30:10 -0900

    improved mask

--- asymter/__init__.py
+++ asymter/__init__.py
@@ -6,7 +6,7 @@ from asymter.paths import path_adem, path_wm, path_indices, path0
 from asymter.watermask import (
     virtual_arctic_watermask, download_arctic_watermask, match_watermask)
 from asymter.ArcticDEM import (
-    read_adem_tile_buffer, adem_tile_available, adem_defversion, adem_definvalid, adem_defres, 
-    adem_tilestr, download_all_adem_tiles)
+    read_adem_tile_buffer, adem_tile_available, adem_defversion, adem_definvalid, 
+    adem_defres, adem_tilestr, download_all_adem_tiles)
 from asymter.terrain import asymter_tile, batch_asymter, asymindex, asymindex_pts
 
